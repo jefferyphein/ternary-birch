105ef11fdfb2640161f979038d77bfdd737e4e29
Jeffery Hein <jefferyphein@gmail.com>
2019-01-26 08:49:24 -0500

    added birch.cpp

---
+++ src/birch.cpp
@@ -0,0 +1,43 @@
+#include "birch.h"
+#include "Genus.h"
+
+int main(int argc, char **argv)
+{
+    std::vector<Z_PrimeSymbol> symbols;
+    Z_PrimeSymbol p;
+
+    p.p = 11;
+    p.power = 1;
+    p.ramified = true;
+    symbols.push_back(p);
+
+    p.p = 13;
+    p.power = 1;
+    p.ramified = true;
+    symbols.push_back(p);
+
+    p.p = 17;
+    p.power = 1;
+    p.ramified = true;
+    symbols.push_back(p);
+
+    p.p = 19;
+    p.power = 1;
+    p.ramified = true;
+    symbols.push_back(p);
+
+    p.p = 23;
+    p.power = 1;
+    p.ramified = true;
+    symbols.push_back(p);
+
+    Z_Isometry s;
+    Z_QuadForm q = Z_QuadForm::get_quad_form(symbols);
+
+    Z_Genus genus1(q, symbols);
+    Z64_Genus genus2 = Z_Genus::convert<Z64>(genus1);
+
+    genus2.hecke_matrix_dense(8191);
+
+    return EXIT_SUCCESS;
+}
