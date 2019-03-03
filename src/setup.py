afeb2a268d2eddecb275c3bd0aa08b323b435733
Jeffery Hein <jefferyphein@gmail.com>
2019-03-03 13:15:22 -0500

    added LICENSE.txt and updated setup.py with more metadata

--- src/setup.py
+++ src/setup.py
@@ -1,8 +1,22 @@
 from distutils.core import setup
+from distutils.extension import Extension
 from Cython.Build import cythonize
+
+extensions = [
+    Extension("ternary_birch", ["ternary_birch.pyx"],
+        libraries=['gmp', 'gmpxx'],
+    )
+]
 
 setup(
     name = "ternary-birch",
     version = "2.0.0-beta.3",
-    ext_modules = cythonize("ternary_birch.pyx")
+    author = "Jeffery Hein",
+    author_email = "jefferyphein@gmail.com",
+    maintainer = "Jeffery Hein",
+    maintainer_email = "jefferyphein@gmail.com",
+    url = "https://github.com/jefferyphein/ternary-birch",
+    description = "A library for computing Hecke matrices, eigenvectors, and eigenvalues for positive definite rational ternary quadratic forms.",
+    license = "GNU GPL",
+    ext_modules = cythonize(extensions)
 )
